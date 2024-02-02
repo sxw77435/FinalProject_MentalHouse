dceb4fee91fda0608f9edcb513164b016dc61cda
xinweiso <sxw6644@gmail.com>
2024-02-02 11:06:24 +0900

    풀더

---
+++ src/main/java/com/uni/mental/config/ContextConfiguration.java
@@ -0,0 +1,9 @@
+package com.uni.mental.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.uni.mental")
+public class ContextConfiguration {
+}
