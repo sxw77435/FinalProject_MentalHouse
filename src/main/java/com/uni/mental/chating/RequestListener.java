c8e9b337d91e8a7ec99b6dc45fa3a0df2323e224
xinweiso <151901881+xinweiso@users.noreply.github.com>
2024-02-21 16:02:03 +0900

    Update RequestListener.java

--- src/main/java/com/uni/mental/chating/RequestListener.java
+++ src/main/java/com/uni/mental/chating/RequestListener.java
@@ -1,21 +1,2 @@
-package com.uni.mental.chating;
 
-import jakarta.servlet.ServletRequestEvent;
-import jakarta.servlet.ServletRequestListener;
-import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.stereotype.Component;
-
-@Component
-public class RequestListener implements ServletRequestListener {
-    @Override
-    public void requestInitialized(ServletRequestEvent sre)  {
-
-        //将所有request请求都携带上httpSession
-        ((HttpServletRequest) sre.getServletRequest()).getSession();
-    }
-    public RequestListener() {}
-
-    @Override
-    public void requestDestroyed(ServletRequestEvent arg0)  {}
-}
 
